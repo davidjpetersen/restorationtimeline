c213d02e4eebbec9d47ea2e3e8300f962253e613
David Petersen <dp@davidp.us>
2021-10-27 11:18:38 -0600

    Add migrations.

--- database/migrations/2021_10_27_034714_create_people_table.php
+++ database/migrations/2021_10_27_034714_create_people_table.php
@@ -15,6 +15,26 @@ class CreatePeopleTable extends Migration
     {
         Schema::create('people', function (Blueprint $table) {
             $table->id();
+            $table->string('honorificPrefix')->nullable();
+            $table->string('givenName')->nullable();
+            $table->string('additionalName')->nullable();
+            $table->string('familyName')->nullable();
+            $table->string('honorificSuffix')->nullable();
+            $table->string('address')->nullable();
+            $table->string('affiliation')->nullable();
+            $table->string('alumniOf')->nullable();
+            $table->dateTime('birthDate')->nullable();
+            $table->string('birthPlace')->nullable();
+            $table->dateTime('deathDate')->nullable();
+            $table->string('deathPlace')->nullable();
+            $table->string('email')->nullable();
+            $table->string('telephone')->nullable();
+            $table->string('faxNumber')->nullable();
+            $table->string('gender')->nullable();
+            $table->integer('height')->nullable();
+            $table->integer('weight')->nullable();
+            $table->json('identifier')->nullable();
+            $table->json('sameAs')->nullable();
             $table->timestamps();
         });
     }
--- database/migrations/2021_10_27_161504_create_organizations_table.php
+++ database/migrations/2021_10_27_161504_create_organizations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePlacesTable extends Migration
+class CreateOrganizationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,26 +13,20 @@ class CreatePlacesTable extends Migration
      */
     public function up()
     {
-        Schema::create('places', function (Blueprint $table) {
+        Schema::create('organizations', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
             $table->string('alternativeName')->nullable();
             $table->text('description')->nullable();
             $table->text('disambiguatingDescription')->nullable();
             $table->text('address')->nullable();
-            $table->text('geo')->nullable();
+            $table->dateTime('foundingDate')->nullable();
+            $table->string('foundingLocation');
+            $table->dateTime('dissolutionDate')->nullable();
+            $table->string('dissolutionLocation');
             $table->string('faxNumber')->nullable();
+            $table->string('email')->nullable();
             $table->string('telephone')->nullable();
-            $table->string('hasMap')->nullable();
-            $table->boolean('isAccessibleForFree')->nullable();
-            $table->string('latitude')->nullable();
-            $table->string('logitude')->nullable();
-            $table->string('logo')->nullable();
-            $table->number('maximumAttendeeCapacity')->nullable();
-            $table->text('openingHoursSpecification')->nullable();
-            $table->boolean('publicAccess')->nullable();
-            $table->string('slogan')->nullable();
-            $table->string('tourBookingPage')->nullable();
             $table->json('identifier')->nullable();
             $table->json('sameAs')->nullable();
             $table->timestamps()->nullable();
@@ -46,6 +40,6 @@ class CreatePlacesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('places');
+        Schema::dropIfExists('organizations');
     }
 }
